9d709aafe0e1a55fabfb0b9afd3591e926723bfe
baptiste <baptiste.auguie@gmail.com>
2015-07-14 09:53:46 +1200

    separator example

--- inst/testing/table.R
+++ inst/testing/table.R
@@ -13,6 +13,15 @@ tt3 <- ttheme_minimal(core=list(bg_params = list(fill = blues9,
                                                  col=NA)), 
                       colhead=list(fg_params=list(col="navyblue")))
 
+grid.newpage()
+g <- tableGrob(iris[1:4, 1:3], theme=tt2)
+separators <- replicate(ncol(g) - 2, 
+                     segmentsGrob(x1=unit(0,"npc")), 
+                     simplify=FALSE)
+g <- gtable::gtable_add_grob(g, grobs = separators, 
+                     t = 1, b = nrow(g), l = seq_len(ncol(g)-2)+2)
+grid.draw(g)
+
 grid.arrange(
   tableGrob(iris[1:4, 1:2], theme=tt1),
   tableGrob(iris[1:4, 1:2], theme=tt2),
